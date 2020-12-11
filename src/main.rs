00da2a069c4fb9f2103555854280c5fc93102f59
Joel Setterberg <j.settrbrg@gmail.com>
2020-12-11 20:09:33 +0100

    Cleaingin in main

--- src/main.rs
+++ src/main.rs
@@ -14,40 +14,45 @@ macro_rules! run_day {
     }};
 }
 
+macro_rules! invalid_day {
+    ($x:expr) => {{
+        println!("Invalid day number: {}", $x);
+    }};
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let mut day = String::new();
 
-    if args.len() >= 2 {
-        day = args[1].clone();
-    } else {
-        println!("Enter day: ");
-        io::stdin()
-            .read_line(&mut day)
-            .expect("Failed to read line");
+    match args.get(1) {
+        Some(option) => {
+            day = format!("{}", option);
+        }
+        None => {
+            println!("Enter day: ");
+            io::stdin()
+                .read_line(&mut day)
+                .expect("Failed to read line");
+        }
     }
 
-    day = day.trim().to_string();
-    let day_num: u8 = match day.parse() {
-        Ok(num) => num,
+    match day.parse::<i32>() {
+        Ok(day_num) => match day_num {
+            1 => run_day!(day1),
+            2 => run_day!(day2),
+            3 => run_day!(day3),
+            4 => run_day!(day4),
+            5 => run_day!(day5),
+            6 => run_day!(day6),
+            7 => run_day!(day7),
+            8 => run_day!(day8),
+            9 => run_day!(day9),
+            10 => run_day!(day10),
+            11 => run_day!(day11),
+            _ => invalid_day!(day),
+        },
         Err(_) => {
-            println!("Invalid day number: {}", day);
-            return;
+            invalid_day!(day);
         }
     };
-
-    match day_num {
-        1 => run_day!(day1),
-        2 => run_day!(day2),
-        3 => run_day!(day3),
-        4 => run_day!(day4),
-        5 => run_day!(day5),
-        6 => run_day!(day6),
-        7 => run_day!(day7),
-        8 => run_day!(day8),
-        9 => run_day!(day9),
-        10 => run_day!(day10),
-        11 => run_day!(day11),
-        _ => println!("Invalid day number: {}", day_num),
-    }
 }
