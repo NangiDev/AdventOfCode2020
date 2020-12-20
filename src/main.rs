2d4fa738a24c2d9538f8270de0c678f849cc017e
Joel Setterberg <j.settrbrg@gmail.com>
2020-12-20 23:44:28 +0100

    Some more fixing

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,20 @@
-use std::env;
+#![feature(array_value_iter)]
+
+use std::{collections::HashMap, env};
 
 use adventofcode_2020::*;
 
-const DAYS: &[fn()] = &[
-    day01::print,
-    day02::print,
-    day03::print,
-    day04::print,
-    day05::print,
-    day06::print,
-    day07::print,
-    day08::print,
-    day09::print,
-    day10::print,
-    day11::print,
-    day12::print,
-    day13::print,
-    day14::print,
-    day15::print,
-    day16::print,
-    day16::print,
-    day18::print,
-    day18::print,
-    day18::print,
-];
+macro_rules! DAYS {
+    ($($k:expr => $v:expr),* $(,)?) => {
+        {
+            let mut m: HashMap<i32, fn()> = ::std::collections::HashMap::new();
+            $(
+                m.insert($k,$v);
+            )*
+            m
+        }
+    };
+}
 
 macro_rules! invalid_day {
     ($x:expr) => {{
@@ -32,11 +23,31 @@ macro_rules! invalid_day {
 }
 
 fn main() {
+    let list: HashMap<i32, fn()> = DAYS! {
+        1 => day01::print,
+        2 => day02::print,
+        3 => day03::print,
+        4 => day04::print,
+        5 => day05::print,
+        6 => day06::print,
+        7 => day07::print,
+        8 => day08::print,
+        9 => day09::print,
+        10 => day10::print,
+        11 => day11::print,
+        12 => day12::print,
+        13 => day13::print,
+        14 => day14::print,
+        15 => day15::print,
+        16 => day16::print,
+        18 => day18::print,
+    };
+
     let args: Vec<String> = env::args().collect();
 
     if let Some(day) = args.get(1) {
-        match day.trim().parse::<usize>() {
-            Ok(day) => match DAYS.get(day.wrapping_sub(1)) {
+        match day.trim().parse::<i32>() {
+            Ok(day) => match list.get(&day) {
                 Some(day) => day(),
                 None => invalid_day!(day),
             },
